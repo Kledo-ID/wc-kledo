ca081dffd0d6e899d5d1823d0806b0d0927d0770
Panji Setya Nur Prawira <kstar.panjinamjaelf@gmail.com>
2021-07-29 09:02:24 +0700

    feat: Toggles invoice field

--- assets/js/invoice.js
+++ assets/js/invoice.js
@@ -1,3 +1,27 @@
 /**
  * Copyright (c) Kledo Software. All Rights Reserved
  */
+
+jQuery(document).ready(function ($) {
+    /**
+     * Toggles availability of input in setting groups.
+     *
+     * @param {boolean} enable whether fields in this group should be enabled or not.
+     */
+    function toggleSettingOptions (enable) {
+        $('.invoice-field').each(function () {
+            let $element = $(this);
+
+            if (enable) {
+                $element.css('pointer-events', 'all').css('opacity', '1.0');
+            } else {
+                $element.css('pointer-events', 'none').css('opacity', '0.4');
+            }
+        });
+    }
+
+    // Disable field if connection status disconnected.
+    if ($('form.wc-kledo-settings').hasClass('disconnected')) {
+        toggleSettingOptions(false);
+    }
+});
