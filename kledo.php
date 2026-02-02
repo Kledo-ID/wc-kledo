f3e938845042ac9a4fe6643f1c9b67abffbc2b9e
Panji Setya Nur Prawira <kstar.panjinamjaelf@gmail.com>
2026-02-02 14:37:43 +0700

    docs: update changelog 👽

--- kledo.php
+++ kledo.php
@@ -6,10 +6,10 @@
  * Description: Integrates <a href="https://woocommerce.com/" target="_blank" >WooCommerce</a> with the <a href="https://kledo.com" target="_blank">Kledo</a> accounting software.
  * Author: Kledo
  * Author URI: https://kledo.com
- * Version: 1.3.1
+ * Version: 1.4.0
  * Text Domain: wc-kledo
  * WC requires at least: 3.5.0
- * WC tested up to: 10.0.4
+ * WC tested up to: 10.4.3
  */
 
 // Exit if accessed directly.
@@ -27,7 +27,7 @@ class WC_Kledo_Loader {
 	 * @var string
 	 * @since 1.0.0
 	 */
-	public const VERSION = '1.3.1';
+	public const VERSION = '1.4.0';
 
 	/**
 	 * Minimum PHP version.
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: kledo
 Tags: Kledo, WooCommerce, Accounting
 Requires at least: 4.4
-Tested up to: 6.8
-Stable tag: 1.3.1
+Tested up to: 6.9
+Stable tag: 1.4.0
 Requires PHP: 7.0.0 or greater
 Text Domain: wc-kledo
 License: GPLv2 or later
@@ -35,6 +35,9 @@ Integrates WooCommerce with Kledo Accounting Software.
 2. The Kledo invoice plugin Settings pages
 
 == Changelog ==
+
+= 1.4.0 =
+* feat: use api key
 
 = 1.3.1 =
 * fix: cannot create new order when status changed to processing
